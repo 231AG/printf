17de1f1d82e4deb9621e6c6d8461fdca976f4af4
Ishmael M. Dukuly <99619343+IshmaeDukuly@users.noreply.github.com>
2023-04-26 12:20:56 +0000

    Update functions.c

--- functions.c
+++ functions.c
@@ -3,7 +3,7 @@
  * print_char - Prints a char
  * @types: List a # of arguments
  * @buffer: Buffer array to handle print
- * @flags:  Calculates active flags
+ * @flags:  The active flags calculator
  * @width: Width
  * @precision: Precision specification
  * @size: Size specifier
