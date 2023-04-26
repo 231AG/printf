fa214cdc8de1ad3cef1761d7dcedd69f8738d0c9
Ishmael M. Dukuly <99619343+IshmaeDukuly@users.noreply.github.com>
2023-04-26 13:07:18 +0000

    Update get_flags.c

--- get_flags.c
+++ get_flags.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
 /**
- * get_flags - Calculates active flags
+ * get_flags - Calculates The active flags
  * @format: Formatted string in which to print the arguments
- * @i: take a parameter.
+ * @i: take The parameter.
  *
  * Return: Flags:
  */
